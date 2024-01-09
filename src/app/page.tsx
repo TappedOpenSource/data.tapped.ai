6a3e1fe14fba8da151e24c37045d74822c8dd43e
Johannes Naylor <jonaylor89@gmail.com>
2024-01-09 14:18:24 -0500

    padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-24">
+    <main className="flex min-h-screen flex-col items-center justify-start p-4 lg:p-24">
         <h1
           className='text-6xl font-extrabold text-center text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'
         >tapped data</h1>
